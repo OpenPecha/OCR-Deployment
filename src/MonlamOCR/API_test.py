14a157cbf85196a75e7c3c74816b9a86527d3e16
ta4tsering <ta3tsering@gmail.com>
2024-08-23 00:18:48 +0530

    update the API_test

--- src/MonlamOCR/API_test.py
+++ src/MonlamOCR/API_test.py
@@ -1,9 +1,6 @@
 import requests
 
-def send_request_to_api(endpoint, data=None, method="POST"):
-    base_url = "http://18.206.160.30:8000"
-    url = f"{base_url}{endpoint}"
-
+def send_request_to_api(url, data):
     try:
         response = requests.post(url, json=data)
         return response.json()['unicode_text']
@@ -11,11 +8,10 @@ def send_request_to_api(endpoint, data=None, method="POST"):
         return {"error": "An error occurred while making the request", "details": str(e)}
 
 
-# For a POST request with JSON data:
 data = {
     "image_url": "https://s3.amazonaws.com/monlam.ai.ocr/Test/input/I8LS169351039.jpg"
 }
-Transcript = send_request_to_api("/process", data=data, method="POST")
+Transcript = send_request_to_api("http://18.206.160.30:8000/process", data)
 print(Transcript)
 
 
